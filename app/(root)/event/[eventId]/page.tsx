6c308fc8bb2290153565a6d38f7b1b17b2a7ec41
Riyad139 <riyad.ahmed1886@gmail.com>
2024-06-01 03:08:14 +0600

    added

--- app/(root)/event/[eventId]/page.tsx
+++ app/(root)/event/[eventId]/page.tsx
@@ -60,7 +60,6 @@ export default async function EventPage({
               placeholder=""
             />
           </div>
-          
           <div>
             <Label htmlFor="tel">Telephone</Label>
             <Input
